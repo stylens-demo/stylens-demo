03913ecfd59cdabd9f91fc7e615d9c6230e72a72
Daesub Kim <rano@bluehack.net>
2018-01-30 15:10:12 +0900

    1. Implemented for Demo. 2. version: 0.0.1 / build: 7

---
+++ stylens-demo/ViewControllers/GalleryView/GalleryViewController.h
@@ -0,0 +1,13 @@
+//
+//  GalleryViewController.h
+//  stylens-demo
+//
+//  Created by 김대섭 on 2018. 1. 28..
+//  Copyright © 2018년 Bluehack Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GalleryViewController : UIViewController
+
+@end
